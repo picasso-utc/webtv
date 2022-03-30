707664b4d41c272c3c183b29c5507da6084b7a54
Adrien Simon <adrien.simon@etu.utc.fr>
2022-03-30 10:28:14 +0200

    problème d'accès aux datas de kraken réglé

--- src/pages/PermVDH.js
+++ src/pages/PermVDH.js
@@ -7,22 +7,26 @@ import './permVDH.css'
 
 const PermVDH = () => {
 
-    const [dons, setDons] = useState({
+    const [beers, setBeers] = useState({
         dons10: {
-            id: 17348,
-            quantity: 0,
+            dons10: {
+                id: 17348,
+                quantity: 0
+            }
         },
 
         dons50: {
-            id: 17349,
-            quantity: 0,
-        },
+            dons50: {
+                id: 17349,
+                quantity: 0
+            }
+        }
     })
 
     const loadDons = () => {
-        ajaxPost('payutc/public/beers/sells', {'dons': dons}).then(
+        ajaxPost('payutc/public/beers/sells', {'beers': beers}).then(
             res => {
-                setDons(res.data.dons)
+                setBeers(res.data.beers)
             }
         )
     }
@@ -40,9 +44,9 @@ const PermVDH = () => {
                 <div class="vdhText">
                     <p><strong>PERM VDH</strong></p>
                     <p>Donnez en faveur du Fil d'Ariane !</p>
-                    <p><span class="montant">{dons.dons10.quantity * 0.1 + dons.dons50.quantity * 0.5}</span> EUROS RECOLTES</p>
+                    <p><span class="montant">{beers.dons10.dons10.quantity * 0.1 + beers.dons50.dons50.quantity * 0.5}</span> EUROS RECOLTES</p>
                 </div>
-                <DonsBar color="#009900"  points={dons.dons10.quantity * 0.1 + dons.dons50.quantity * 0.5} objectif={500}/>
+                <DonsBar color="#009900"  points={beers.dons10.dons10.quantity * 0.1 + beers.dons50.dons50.quantity * 0.5} objectif={500}/>
             </div>
         </Grid>
     )
