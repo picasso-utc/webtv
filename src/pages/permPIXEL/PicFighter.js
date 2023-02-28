c57ba46efaf3c951d501855c2ac54e5599dd7511
yuyujijin <eugenevalty@gmail.com>
2023-02-28 15:38:13 +0100

    now handling attack, require aniamtions

--- src/pages/permPIXEL/PicFighter.js
+++ src/pages/permPIXEL/PicFighter.js
@@ -1,70 +1,127 @@
-import React, { useState, useEffect } from 'react'
-import { ajaxPost } from '../../utils/Ajax'
-import Grid from "@material-ui/core/Grid";
-import './PicFighter.css'
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { ajaxPost } from '../../utils/Ajax';
+import { asset_url } from '../../utils/Config';
+import Grid from '@material-ui/core/Grid';
 
 
-const PicFighter = () => {
-    const [player1Health, setPlayer1Health] = useState(100);
-    const [player2Health, setPlayer2Health] = useState(100);
-    const [animation, setAnimation] = useState('idle');
+class PicFighter extends React.Component {
 
-    const [drinks, setDrinks] = useState([
-            {
-                id: 17348,
-                title: "Bonne bière 1",
-                qtty: 0,
-                total: 0,
-                team: "left"
-            },
-            {
-                id: 17349,
-                title: "Bonne bière 2",
-                qtty: 0,
-                total: 0,
-                team: "right"
-            }
-        ]
-    )
+	constructor(props) {
+        super(props);
+        this.state = {
+            drinks : [
+                {
+                    id: 17348,
+                    title: "Bonne bière 1",
+                    total: 0,
+                    team: "left"
+                },
+                {
+                    id: 17349,
+                    title: "Bonne bière 2",
+                    total: 0,
+                    team: "right"
+                }
+            ],
+            attackQueue : [],
+            attacking : false
+        }
+	}
 
-    const loadDrinks = () => {
-        ajaxPost('payutc/public/drinks/sells', {'drinks' : drinks}).then(
+    componentDidUpdate(prevProps, prevState) {
+        // Typical usage (don't forget to compare props):
+        if (prevState.attackQueue !== this.attackQueue) {
+            this.handleAttack();
+        }
+      }
+
+	componentDidMount(){
+        this.loadDrinks();
+        setInterval(() => this.loadDrinks(), 10 * 1000);
+    }
+
+    sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+
+    handleAttack(){
+        if(this.state.attacking) return;
+        if(this.state.attackQueue.length <= 0) return;
+
+        // LOCK
+        this.state.attacking = true;
+
+        // RETRIEVE AND UPDATE
+        let tempAttackQueue = this.state.attackQueue;
+        const attack = tempAttackQueue.shift();
+        this.setState({
+            attackQueue : tempAttackQueue
+        })
+
+        // ATTACK
+        this.playAttackAnimation(attack);
+
+        this.sleep(2000).then(r => {
+            // UNLOCK
+            this.state.attacking = false;
+
+            // if attack queue is not empty
+            this.handleAttack();
+        })
+    }
+
+    playAttackAnimation(attack) {
+        console.log(attack);
+    }
+
+    loadDrinks(){
+        ajaxPost('payutc/public/drinks/sells', {'drinks' : this.state.drinks}).then(
             res => {
-                setDrinks(res.data.drinks)
+
+                for(let i = 0; i < this.state.drinks.length; i++) {
+                    const delta = res.data.drinks[i].total - this.state.drinks[i].total;
+                    
+                    for(let j = 0; j < delta; j++)
+                        this.state.attackQueue.push(this.state.drinks[i].team)
+                }
+
+                this.setState({
+                    drinks : res.data.drinks
+                });
             }
         )
     }
 
-    useEffect(() => { 
-        updateDrinks() 
-    }, [drinks])
+	render() {
 
-    const updateDrinks = () => {
-        /*
-        for(let i = 0; i < drinks.length; i++) {
-            for(drinks[i].qtty; drinks[i].qtty < drinks[i].total; drinks[i].qtty++) {
-                if(drinks[i].team === "left") 
-                    console.log("left");
-                else
-                    console.log("right");
-            }
-        }
-        */
-    }
+        const { classes } = this.props;
+        const { drinks, attackQueue } = this.state;
+        const listItems = attackQueue.map((d) => <li>{d}</li>);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            loadDrinks()
-        }, 10 * 1000);
+		return (
+            
+            <Grid className="mainContainerPicFighter">
+                <p>
+                    {drinks[0].id}
+                </p>
+                <p>
+                    {drinks[0].title}
+                </p>
+                <p>
+                    {drinks[0].total}
+                </p>
 
-        return () => clearInterval(interval);
-    }, [])
+                <div>
+                    {listItems }
+                </div>
+            </Grid>   
 
-    return (
-        <Grid className="mainContainerPicFighter">
-            <p>Blablablou</p>
-        </Grid>
-    )
+		);
+	}
 }
 
-export default PicFighter
+const styles = theme => ({});
+
+export default withStyles (styles) (PicFighter)
+
