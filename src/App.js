b15870ae17cbe7c5a20f08197165cd87de031824
Fanthommm <mcfanthom@gmail.com>
2023-03-23 14:40:37 +0100

    je repare mes betises de git

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ import PermIF from './pages/permIF/permIF';
 import EloRanking from './pages/eloRanking/EloRanking';
 import PicFighter from './pages/permPIXEL/PicFighter';
 import Barbar from './pages/Barbar/Barbar';
+import CheerUT from './pages/cheerUT/cheerUT';
+import JeudiRouge from './pages/JeudiRouge/JeudiRouge';
 
 
 const PUBLIC_URL = process.env.PUBLIC_URL;
@@ -45,6 +47,8 @@ class App extends React.Component {
 					<Route path="/telethon" exact component={Telethon}/>
 					<Route path="/picfighter" exact component={PicFighter}/>
 					<Route path="/barbar" exact component={Barbar}/>
+					<Route path="/cheerUT" exact component={CheerUT}/>
+					<Route path="/jeudirouge" exact component={JeudiRouge}/>
 				</Switch>
 			</BrowserRouter>
 		)
