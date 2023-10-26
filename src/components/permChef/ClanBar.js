12631a63d18cf3c611ead60e9b22e1705297277b
geo saglio <geosaglio@saglioge01-hz.etu.utc>
2023-10-26 11:06:29 +0200

    Changer visu perm chef

--- src/components/permChef/ClanBar.js
+++ src/components/permChef/ClanBar.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import './clanBar.css'
+import React from "react";
+import "./clanBar.css";
 
 const ClanBar = ({ logo, color, points, maxPoints }) => {
-    console.log(points, maxPoints)
-    const height = 10 + (points / maxPoints) * 70
-    return (
-        <div className='barContainer clan'>
-            <div style={{
-                width: '100px',
-                height: String(height)+'vh',
-                backgroundColor: color,
-                borderTopRightRadius: '5px',
-                borderTopLeftRadius: '5px',
-            }}/>
-            <img src={logo} alt={"Logo de " + color} className='logoBar'/>
-        </div>
-    )
-}
+  console.log(points, maxPoints);
+  const height = 10 + (points / maxPoints) * 70;
+  return (
+    <div className="chefBarContainer clan">
+      <div
+        style={{
+          width: "100px",
+          height: String(height) + "vh",
+          backgroundColor: color,
+          borderTopRightRadius: "5px",
+          borderTopLeftRadius: "5px",
+        }}
+      />
+      <img
+        src={logo}
+        alt={"Logo de " + color}
+        className="chefLogoBar"
+        style={{ backgroundColor: color }}
+      />
+    </div>
+  );
+};
 
-export default ClanBar
+export default ClanBar;
--- src/pages/permChef/PermChef.js
+++ src/pages/permChef/PermChef.js
@@ -1,108 +1,153 @@
-import React, { useState, useEffect } from 'react'
-import { ajaxPost } from '../../utils/Ajax'
-import Grid from '@material-ui/core/Grid'
-import ClanBar from '../../components/permChef/ClanBar'
-import logoKB from '../../assets/permChef/logoKB.png'
-import logoVB from '../../assets/permChef/logoVB.png'
-import logoYoua from '../../assets/permChef/logoYoua.png'
-import logoTampi from '../../assets/permChef/logoTampi.png'
-import logoDuri from '../../assets/permChef/logoDuri.jpg'
-import logoDagul from '../../assets/permChef/logoDagul.jpg'
-import './permChef.css'
+import React, { useState, useEffect } from "react";
+import { ajaxPost } from "../../utils/Ajax";
+import Grid from "@material-ui/core/Grid";
+import ClanBar from "../../components/permChef/ClanBar";
+import logoHarpona from "../../assets/permChef/harpona.png";
+import logoOrphies from "../../assets/permChef/orphies.png";
+import logoEnemagoy from "../../assets/permChef/enemagoy.png";
+import logoVurion from "../../assets/permChef/vurion.png";
+import logoDuri from "../../assets/permChef/logoDuri.jpg";
+import logoDagul from "../../assets/permChef/logoDagul.jpg";
+import "./permChef.css";
 
 const PermChef = () => {
-    // Cuvée =>  457
-    // Deli => 458
-    // Kastell => 14821
-    // Cidre => 16581
-    // Val Dieu => 12492
-    // Castor => 16755
-    // Harmony => 12463
-    // Chimay Rouge => 16870
-    // Barbar => 1403
-    const [beers, setBeers] = useState({
-        tampi: {
-            barbar: {
-                id: 1403,
-                quantity: 0
-            }
-        },
-        kb: {
-            castor: {
-                id: 16755,
-                quantity: 0
-            }
-        },
-        youa: {
-            valdieu: {
-                id: 12492,
-                quantity: 0
-            }
-        },
-        vb: {
-            cidre: {
-                id: 17835,
-                quantity: 0
-            }
-        },
-        duri: {
-            delirium: {
-                id: 458,
-                quantity: 0
-            }
-        },
-        dagul: {
-            cuvee: {
-                id: 457,
-                quantity: 0
-            }
-        }
-    })
+  const [beers, setBeers] = useState(
+    [
+      {
+        id: 19778,
+        title: "Tarot d’or",
+        total: 0,
+      },
+      {
+        id: 14821,
+        title: "Kasteel Rouge",
+        total: 0,
+      },
+      {
+        id: 457,
+        title: "Cuvée des Trolls",
+        total: 0,
+      },
+      {
+        id: 17835,
+        title: "Cidre La Mordue",
+        total: 0,
+      },
+      {
+        id: 19824,
+        title: "Delirium",
+        total: 0,
+      },
+      {
+        id: 12492,
+        title: "Valdieu",
+        total: 0,
+      },
+    ],
+    /*orphi: {
+      kastel: {
+        id: 17152,
+        quantity: 0,
+      },
+    },
+    enema: {
+      valdieu: {
+        id: 12492,
+        quantity: 0,
+      },
+    },
+    harpo: {
+      tarotDor: {
+        id: 19778,
+        quantity: 0,
+      },
+    },
+    vurion: {
+      cidre: {
+        id: 17835,
+        quantity: 0,
+      },
+    },
+    duri: {
+      delirium: {
+        id: 19824,
+        quantity: 0,
+      },
+    },
+    dagul: {
+      cuvee: {
+        id: 457,
+        quantity: 0,
+      },
+    },*/
+  );
 
-    const [maxPoints, setMaxPoints] = useState(1)
+  const [maxPoints, setMaxPoints] = useState(1);
 
-    const loadBeerSells = () => {
-        ajaxPost('payutc/public/beers/sells', {'beers': beers}).then(
-            res => {
-                setBeers(res.data.beers)
-            }
-        )
-    }
+  const loadBeerSells = () => {
+    ajaxPost("payutc/public/drinks/sells", { drinks: beers }).then((res) => {
+      console.log(res.data);
+      setBeers(res.data.drinks);
+    });
+  };
 
-    useEffect(() => {
-        setMaxPoints(Math.max(
-            beers.duri.delirium.quantity,
-            beers.kb.castor.quantity,
-            beers.vb.cidre.quantity,
-            beers.tampi.barbar.quantity,
-            beers.youa.valdieu.quantity,
-            beers.dagul.cuvee.quantity,
-            1
-        ))
-    }, [beers])
+  useEffect(() => {
+    setMaxPoints(Math.max(...beers.map((obj) => obj.total), 1));
+  }, [beers]);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            loadBeerSells()
-        }, 60 * 1000);
-        return () => clearInterval(interval);
-    }, [])
+  useEffect(() => {
+    loadBeerSells();
+    const interval = setInterval(() => {
+      loadBeerSells();
+    }, 15 * 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <Grid class="mainContainer">
-            <div className='blurEffect'>
-                <div className='header'>PERM CHEFS</div>
-                <div className='bars'>
-                    <ClanBar logo={logoDuri} color="#ffffff" points={(beers.duri.delirium.quantity) * 1000} maxPoints={maxPoints * 1000} />
-                    <ClanBar logo={logoKB} color="#ff0000" points={(beers.kb.castor.quantity) * 1000} maxPoints={maxPoints * 1000} />
-                    <ClanBar logo={logoVB} color="#00ff00" points={(beers.vb.cidre.quantity) * 1000} maxPoints={maxPoints * 1000} />
-                    <ClanBar logo={logoTampi} color="#ffff00" points={(beers.tampi.barbar.quantity) * 1000} maxPoints={maxPoints * 1000} />
-                    <ClanBar logo={logoYoua} color="#0000ff" points={(beers.youa.valdieu.quantity) * 1000} maxPoints={maxPoints * 1000} />
-                    <ClanBar logo={logoDagul} color="#000000" points={(beers.dagul.cuvee.quantity) * 1000} maxPoints={maxPoints * 1000} />
-                </div>
-            </div>
-        </Grid>
-    )
-}
+  return (
+    <Grid class="chefMainContainer">
+      <div className="chefBlurEffect">
+        <div className="chefHeader">PERM CHEFS</div>
+        <div className="chefBars">
+          <ClanBar
+            logo={logoDuri}
+            color="#ffffff"
+            points={beers?.find((x) => x.id == 19824)?.total * 1000}
+            maxPoints={maxPoints * 1000}
+          />
+          <ClanBar
+            logo={logoEnemagoy}
+            color="#821A89"
+            points={beers?.find((x) => x.id == 12492)?.total * 1000}
+            maxPoints={maxPoints * 1000}
+          />
+          <ClanBar
+            logo={logoVurion}
+            color="#003366"
+            points={beers?.find((x) => x.id == 17835)?.total * 1000}
+            maxPoints={maxPoints * 1000}
+          />
+          <ClanBar
+            logo={logoOrphies}
+            color="#AC3C13"
+            points={beers?.find((x) => x.id == 14821)?.total * 1000}
+            maxPoints={maxPoints * 1000}
+          />
+          <ClanBar
+            logo={logoHarpona}
+            color="#FFD700"
+            points={beers?.find((x) => x.id == 19778)?.total * 1000}
+            maxPoints={maxPoints * 1000}
+          />
+          <ClanBar
+            logo={logoDagul}
+            color="#000000"
+            points={beers?.find((x) => x.id == 457)?.total * 1000}
+            maxPoints={maxPoints * 1000}
+          />
+        </div>
+      </div>
+    </Grid>
+  );
+};
 
-export default PermChef
+export default PermChef;
