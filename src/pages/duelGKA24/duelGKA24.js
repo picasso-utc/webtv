acbb2a5fa40f2a5767cabb7cb59299b8927ba6a0
VictorD <victor.demessance@gmail.com>
2024-09-05 14:37:36 +0200

    Duel Gnome Knight A24

--- src/pages/duelGKA24/duelGKA24.js
+++ src/pages/duelGKA24/duelGKA24.js
@@ -4,69 +4,56 @@ import { ajaxPost } from '../../utils/Ajax';
 import { asset_url } from '../../utils/Config';
 import './duelGKA24.css';
 
+const CUVEE_ID = 457;
+const RASTA_ID = 20383;
+const LUPULUS_ID = 20500;
+const VALDIEU_ID = 12492;
+const STFEUILLIEN_ID = 890;
+const RUBUS_ID = 20417;
+
+const MAX_JAUGE_PX = 375;
+
 // Duel Gnome Knight A24
 
-
 class duelGKA24 extends React.Component {
+
 
     constructor(props) {
         super(props);
 
-        const PTS_GNOME_ID = 19812;
-        const PTS_KNIGHT_ID = 19811;
-        const CUVEE_ID = 19820;
-        const RASTA_ID = 19817;
-        const LUPULUS_ID = 19815;
-        const VALDIEU_ID = 19822;
-        const ANGELUS_ID = 19816;
-        const RUBUS_ID = 19819;
-
-    
         this.state = {
             drinks : [
                 {
-                    id: PTS_GNOME_ID,
-                    title : 'PtsGnome',
-                    total: 0
-                },
-                {
-                    id: PTS_KNIGHT_ID,
-                    title : 'PtsKnight',
-                    total: 0
-                },
-                {
                     id: CUVEE_ID,
-                    title : 'cuvee',
+                    title : 'Cuvee',
                     total: 0
                 },
                 {
                     id: RASTA_ID,
-                    title : 'rasta',
+                    title : 'Rasta',
                     total: 0
                 },
                 {
                     id: LUPULUS_ID,
-                    title : 'lupulus',
+                    title : 'Lupulus',
                     total: 0
                 },
                 {
                     id: VALDIEU_ID,
-                    title : 'valdieu',
+                    title : 'Valdieu',
                     total: 0
                 },
                 {
-                    id: ANGELUS_ID,
-                    title : 'angelus',
+                    id: STFEUILLIEN_ID,
+                    title : 'St_feuillien',
                     total: 0
                 },
                 {
                     id: RUBUS_ID,
-                    title : 'rubus',
+                    title : 'Rubus',
                     total: 0
                 },
             ],
-            quantitéL : 135,
-            ventes : 0
         }
     }
 
@@ -92,66 +79,62 @@ class duelGKA24 extends React.Component {
     }
 
     render() {
-        const qty = this.state.quantitéL
-        const ventes = this.state.ventes
         
-        const PtsGnome = this.state.drinks.find(x => x.id === PTS_GNOME_ID).total;
-        const PtsKnight = this.state.drinks.find(x => x.id === PTS_KNIGHT_ID).total;
         const cuvee = this.state.drinks.find(x => x.id === CUVEE_ID).total;
         const rasta = this.state.drinks.find(x => x.id === RASTA_ID).total;
         const lupulus = this.state.drinks.find(x => x.id === LUPULUS_ID).total;
         const valdieu = this.state.drinks.find(x => x.id === VALDIEU_ID).total;
-        const angelus = this.state.drinks.find(x => x.id === ANGELUS_ID).total;
+        const stfeuillien = this.state.drinks.find(x => x.id === STFEUILLIEN_ID).total;
         const rubus = this.state.drinks.find(x => x.id === RUBUS_ID).total;
 
-
+        const PtsGnome = cuvee + rasta + lupulus
+        const PtsKnight = valdieu + stfeuillien + rubus
+       
+        const max_biere = Math.max(cuvee, rasta, lupulus, valdieu, stfeuillien, rubus) 
+        
         return (
             <div className='body'>
                 <div className='cuvee'>
-                    <div className='jauge' style={{width: 487-(cuvee*2.95)}}>
-                        <p className='Nb_Gauche' >{165-cuvee}</p>
+                    <div className='jauge' style={{width: (cuvee * MAX_JAUGE_PX / max_biere)}}>
+                        <p className='Nb_Gauche' >{cuvee}</p>
                     </div>
                 </div>
                 <div className='rasta'>
-                    <div className='jauge' style={{width: 487-(rasta*6)}}>
-                        <p className='Nb_Gauche'>{81-rasta}</p>
+                    <div className='jauge' style={{width: (rasta * MAX_JAUGE_PX / max_biere)}}>
+                        <p className='Nb_Gauche'>{rasta}</p>
                     </div>
                 </div>
                 <div className='lupulus'>
-                    <div className='jauge' style={{width: 487-(lupulus*2.95)}}>
-                        <p className='Nb_Gauche'>{165-lupulus}</p>
+                    <div className='jauge' style={{width: (lupulus * MAX_JAUGE_PX / max_biere)}}>
+                        <p className='Nb_Gauche'>{lupulus}</p>
                     </div>
                 </div>
                 <div className='valdieu'>
-                    <div className='jauge' style={{width: 487-(valdieu*2.7)}}>
-                        <p className='Nb_Droit'>{165-valdieu}</p>
+                    <div className='jauge' style={{width: (valdieu * MAX_JAUGE_PX / max_biere)}}>
+                        <p className='Nb_Droit'>{valdieu}</p>
                     </div>
                 </div>
-                <div className='angelus'>
-                    <div className='jauge' style={{width: 487-(angelus*2.7)}}>
-                        <p className='Nb_Droit' >{53-angelus}</p>
+                <div className='stfeuillien'>
+                    <div className='jauge' style={{width: (stfeuillien * MAX_JAUGE_PX / max_biere)}}>
+                        <p className='Nb_Droit'>{stfeuillien}</p>
                     </div>
                 </div>
                 <div className='rubus'>
-                    <div className='jauge' style={{width: 487-(rubus*2.95)}}>
-                        <p className='Nb_Droit' >{165-rubus}</p>
+                    <div className='jauge' style={{width: (rubus * MAX_JAUGE_PX / max_biere)}}>
+                        <p className='Nb_Droit' >{rubus}</p>
                     </div>
                 </div>
-
                 <div className='PtsGnome'>
                     <p>{PtsGnome}</p>
                 </div>
-
                 <div className='PtsKnight'>
                     <p>{PtsKnight}</p>
                 </div>
-                    <img className="bg" src={asset_url("/images/duelDesBrasseursP23/duelSillyDeliBG.png")}></img>
+                <img className="bg" src={asset_url("/images/fond_GKA24.png")}></img>
             </div>
         )
     }
 
 }
-
-
 
 export default duelGKA24
