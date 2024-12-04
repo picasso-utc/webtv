51544e277d8e70375ce1bd9c5eb6b8b244a7b231
VictorD <victor.demessance@gmail.com>
2024-12-04 16:15:15 +0100

    Ajout de la mise et clarification du code

--- src/pages/RDPA24/aviator.js
+++ src/pages/RDPA24/aviator.js
@@ -3,54 +3,94 @@ import "./aviator.css";
 import { asset_url } from '../../utils/Config';
 
 const Aviator = () => {
-  const [multiplier, setMultiplier] = useState(1.0);
-  const [running, setRunning] = useState(false);
-  const [result, setResult] = useState("");
+  // Définition des hooks
+  const [score, setScore] = useState(1.0);        // Initalisation du score à 1
+  const [mise, setMise] = useState(0);            // Mise de départ à 0
+  const [ranking, setRanking] = useState([]);     // Classement de départ vide
+  const [running, setRunning] = useState(false);  // Le jeu attend d'être lancé
+  const [result, setResult] = useState("");       // Variable de résultat
 
+  // Définition des variables du modèle statistique
+  let increament_step = 0.1;
+  const proba_rate = 0.01;
+  const timestep = 200;
+
+  // Définition de l'effet de variation du score
   useEffect(() => {
     if (!running) return;
 
     const interval = setInterval(() => {
-      setMultiplier((prev) => {
-        const newMultiplier = prev + 0.1;
-        if (Math.random() < 0.01   * newMultiplier) {
+      setScore((prev) => {
+        const newMultiplier = prev + increament_step;
+        if (Math.random() < proba_rate * newMultiplier) {
           clearInterval(interval);
           setRunning(false);
+          setMise(0);
           setResult("Crash ! Vous avez perdu !");
         }
         return newMultiplier;
       });
-    }, 100);
+    }, timestep);
 
     return () => clearInterval(interval);
   }, [running]);
 
+  
+  // Récupération du gain 
   const handleCashout = () => {
     setRunning(false);
-    setResult(`Vous avez encaissé à x${multiplier.toFixed(2)} !`);
+    setResult(`Vous avez encaissé à x${score.toFixed(0)} !`);
+    setMise(mise*score.toFixed(0))
   };
 
+
+  // Gestion de la mise
+  const handleSetMise = (userMise) => {
+    if (!isNaN(userMise) && userMise > 0) {
+      setMise(userMise);
+    }
+  };
+
+  // Démarrage du jeu
   const startGame = () => {
-    setMultiplier(1.0);
+    setScore(1.0);
     setResult("");
     setRunning(true);
   };
 
+
+  // Render
   return (
     <div className="aviator-body">
       <div className={`game-container ${running ? "running" : ""}`}>
+        <div className="mise">
+          Mise actuelle
+          <input
+            className="miseinput"
+            type="number"
+            value={mise}
+            onChange={(e) => {
+              const newMise = parseFloat(e.target.value); 
+              setMise(newMise);
+            }}
+            onWheel={(e) => e.preventDefault()}
+          />
+        </div>
         <div className={`ground ${running ? "running" : ""}`}/>
         <div className="panel">
           <div className="slot">
-            x{multiplier.toFixed(2)}
+            x{score.toFixed(0)}
           </div>
           <div className="button-raw">
-          <button className="button" onClick={handleCashout} disabled={!running}>
-            Encaisser
-          </button>
-          <button className="button" onClick={startGame} disabled={running}>
-            Rejouer
-          </button>
+          {
+            running && <button className="button" onClick={handleCashout}>
+              Encaisser
+            </button>}
+          {
+            !running && <button className="button" onClick={startGame}>
+              Jouer
+            </button>
+          }
         </div>
         </div>
         <div className={`airplane-container ${running ? "running" : ""}`}>
